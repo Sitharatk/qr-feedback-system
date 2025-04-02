b5bd3c20395ce9f477e4d5a7f990c49505582a58
Fathima Sithara <sitharaaneestk@gmail.com>
2025-04-03 03:45:38 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+    origin:process.env.CLIENT_URL,
+    credentials:true,
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
 
 app.use("/api/feedback", feedbackRoutes);
 
